13dd5345146e6236896c5d4d48c6a9589a540167
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-07-11 19:50:26 -0400

    Renamed Object to Entity

---
+++ Missile_Game/app/src/main/java/com/example/lucas/missile_game/Entity.java
@@ -0,0 +1,12 @@
+package com.example.lucas.missile_game;
+
+/**
+ * Created by lucas on 7/11/2016.
+ */
+public class Entity {
+    Vector position;
+
+    public Entity (Vector position){
+        this.position = position;
+    }
+}
