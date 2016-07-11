b04b6ee798a30cbbd9daccb04e1c2686502d888f
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-07-11 19:47:36 -0400

    Added object and vector classes

---
+++ Missile_Game/app/src/main/java/com/example/lucas/missile_game/Object.java
@@ -0,0 +1,10 @@
+package com.example.lucas.missile_game;
+
+/**
+ * Created by lucas on 7/11/2016.
+ */
+public class Object {
+    public Object (){
+
+    }
+}
---
+++ Missile_Game/app/src/main/java/com/example/lucas/missile_game/Vector.java
@@ -0,0 +1,16 @@
+package com.example.lucas.missile_game;
+
+/**
+ * Created by lucas on 7/11/2016.
+ */
+public class Vector {
+    float x;
+    float y;
+
+    public Vector(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
