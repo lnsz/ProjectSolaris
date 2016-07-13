d884bcdf296c85af81df30471777fb9d7ba8d878
HenriqueMatulis <iaminyourgroup@gmail.com>
2016-07-13 17:15:32 -0400

    Fixed typo

--- Missile_Game/app/src/main/java/com/example/lucas/missile_game/EntitySystem.java
+++ Missile_Game/app/src/main/java/com/example/lucas/missile_game/EntitySystem.java
@@ -11,24 +11,24 @@ import java.util.Random;
  */
 public class EntitySystem {
     private ArrayList<Entity> entities;
-    private ArrayList<Entity> entitieBuffer;
+    private ArrayList<Entity> entityBuffer;
     private Iterator<Entity> it;
     public Random generator;
     public EntitySystem(){
         entities = new ArrayList<Entity>();
-        entitieBuffer = new ArrayList<Entity>();
+        entityBuffer = new ArrayList<Entity>();
         generator = new Random();
     }
 
 
     public void addEntity(Entity entity){
-
-        entitieBuffer.add(entity);
+        entityBuffer.add(entity);
     }
 
     void run(Canvas canvas) {
-        entities.addAll(entitieBuffer);
-        entitieBuffer.clear();
+        entities.addAll(entityBuffer);
+        entityBuffer.clear();
+
         it= entities.iterator();
         while (it.hasNext()) {
             Entity p = it.next();
