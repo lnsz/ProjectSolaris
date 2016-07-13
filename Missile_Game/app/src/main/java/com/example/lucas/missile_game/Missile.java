0e9c80570dbef69aabaf1fbf26458d0dff3c0a63
HenriqueMatulis <iaminyourgroup@gmail.com>
2016-07-13 16:45:40 -0400

    Updated missiles draw method
    Is now a triangle with particles shooting out the back, previously was a
    circle

--- Missile_Game/app/src/main/java/com/example/lucas/missile_game/Missile.java
+++ Missile_Game/app/src/main/java/com/example/lucas/missile_game/Missile.java
@@ -2,6 +2,7 @@ package com.example.lucas.missile_game;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 /**
  * Created by Kiko on 11/07/2016.
@@ -9,9 +10,12 @@ import android.graphics.Paint;
 public class Missile extends Entity{
     Paint paint;
     float radius;
+    EntitySystem entities;
 
-    public Missile(float locX, float locY, float tarX, float tarY, float str){
+    public Missile(float locX, float locY, float tarX, float tarY, float str, EntitySystem entities_){
         super(locX, locY);
+        //get entities array
+        entities=entities_;
 
         // Set initial velocity of missile
         velocity = new Vector(tarX, tarY);
@@ -28,8 +32,28 @@ public class Missile extends Entity{
         paint.setARGB(255,255,255,255);
     }
 
+
     public void display(Canvas canvas){
-        canvas.drawCircle(this.location.x, this.location.y, this.radius, this.paint);
+        float angle = velocity.getHeading();
+
+        Path path = new Path();
+        Vector diff = new Vector(0, 2 * radius);
+        diff.rotate(angle);
+        path.moveTo(location.x + diff.x, location.y + diff.y);
+
+        diff = new Vector(-radius,-radius*2);
+        diff.rotate(angle);
+        path.lineTo(location.x +diff.x, location.y + diff.y);
+
+        diff = new Vector(radius,-radius*2);
+        diff.rotate(angle);
+        path.lineTo(location.x + diff.x, location.y + diff.y);
+
+        path.close();
+        canvas.drawPath(path, paint);
+        Particle p = new Particle(location.x, location.y, 10, entities.generator);
+        p.velocity= Vector.mult(this.velocity, -1);
+        entities.addEntity(p);
     }
 
     @Override
