0a08a0b810cab94478ae524b3c809a413c578ffc
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-07-25 18:09:43 -0400

    Added classes back to android

---
+++ android/src/com/mygdx/game/Obstacle.java
@@ -0,0 +1,32 @@
+package com.mygdx.game;
+
+/**
+ * Created by lucas on 7/13/2016.
+ */
+public class Obstacle extends Entity{
+    enum Type{PLANET, MOON, ASTEROID, COMET
+    }
+    boolean gravity;
+    Type type;
+    public Obstacle(Type type, float locX, float locY, float radius){
+        super(locX, locY);
+        this.type = type;
+        this.radius = radius;
+        if (type == Type.PLANET || type == Type.MOON){
+            gravity = true;
+            mass = (float)Math.pow(radius, 3);
+        }
+        else{
+            gravity = false;
+        }
+    }
+
+    public void gravity (Entity other){
+        Vector newAcceleration =  new Vector(this.location.x - other.location.x, this.location.y - other.location.y);
+        float distanceSq = newAcceleration.magSq();
+        newAcceleration.normalize();
+        newAcceleration.mult((float)(1E-1 * this.mass / distanceSq));
+        other.acceleration.add(newAcceleration);
+    }
+
+}
