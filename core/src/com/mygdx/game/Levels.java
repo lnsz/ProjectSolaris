930ae79dc71779e90b36921315dd9e0b204e8268
Lucas Souza <lucas.lunasouza@mail.utoronto.ca>
2016-08-27 00:42:24 -0400

    Level class

---
+++ core/src/com/mygdx/game/Levels.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+/**
+ * Created by lucas on 2016-08-27.
+ */
+public class Levels {
+    public Levels(){
+    }
+    public static void createLevel(int level){
+        switch(level){
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+
+            case 10:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
